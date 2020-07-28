6b55d85f8b475cd882138471b9bf865f5e694b4a
MayaRanjith1 <maya.ranjith07@gmail.com>
2020-07-28 22:55:55 +0530

    Initial Commit

---
+++ ProficiencyExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProficiencyExercise
+//
+//  Created by Ranjith Karuvadiyil on 28/07/20.
+//  Copyright © 2020 Mistybits Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
