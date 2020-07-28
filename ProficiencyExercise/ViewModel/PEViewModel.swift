d9740a09cea5ad04024fa712d75746a5b0aabd1a
MayaRanjith1 <maya.ranjith07@gmail.com>
2020-07-28 23:05:26 +0530

    Added view model file

---
+++ ProficiencyExercise/ViewModel/PEViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PEViewModel.swift
+//  ProficiencyExercise
+//
+//  Created by Ranjith Karuvadiyil on 28/07/20.
+//  Copyright © 2020 Mistybits Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class PEViewModel: NSObject {
+
+}
