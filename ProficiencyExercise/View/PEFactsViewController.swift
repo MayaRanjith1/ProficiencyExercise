dcca31377536d10be58a18a792816825c07a768c
MayaRanjith1 <maya.ranjith07@gmail.com>
2020-07-28 23:01:25 +0530

    Added  Facts view-controller

---
+++ ProficiencyExercise/View/PEFactsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PEFactsViewController.swift
+//  ProficiencyExercise
+//
+//  Created by Ranjith Karuvadiyil on 28/07/20.
+//  Copyright © 2020 Mistybits Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class PEFactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
